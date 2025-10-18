3d5191f0cc44ec2150ff9d55f47d2e7e1c23359a
nlhchinh <chinhnguyen1692000z@gmail.com>
2025-10-19 05:03:23 +0700

    fix

--- src/OrderService/OrderService.Infrastructure/obj/Debug/net8.0/OrderService.Infrastructure.AssemblyInfo.cs
+++ src/OrderService/OrderService.Infrastructure/obj/Debug/net8.0/OrderService.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderService.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45c143e46f71d93c97240d3e6e23912a0d80ca11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+324f2f53ec3773ade05a85e2f2e7bd2380f9d02f")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderService.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderService.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
